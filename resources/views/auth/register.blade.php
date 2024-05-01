34e2117027553b178d0b11052fd7974c0db547e8
terraplanista <terra@planetoide.net>
2024-05-01 00:30:27 -0500

    feat inertia
    ta potente el react con inertia para las rutas xd;

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,7 +64,8 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Register now!') }}
+                                    <!-- Register Now! -->
                                 </button>
                             </div>
                         </div>
